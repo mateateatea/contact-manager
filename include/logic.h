dc08e6b8891d12b9537cb7aea05435f87b8891f8
mateateatea <213237051+mateateatea@users.noreply.github.com.>
2026-03-30 15:18:55 +0200

    structs.h and logic.h closes #5

--- include/logic.h
+++ include/logic.h
@@ -2,4 +2,11 @@
 #define LOGIC_H
 #include "structs.h"
 
+int contact_add(struct ContactArray *arr, struct Contact c);
+int contact_delete(struct ContactArray *arr, int index);
+int contact_edit(struct ContactArray *arr, int index, struct Contact new_data);
+void contact_sort(struct ContactArray *arr);
+int contact_search(struct ContactArray *arr, char *first_name, char *last_name);
+void contact_show(struct ContactArray *arr);
+
 #endif
--- include/structs.h
+++ include/structs.h
@@ -2,4 +2,22 @@
 #define STRUCTS_H
 #include <time.h>
 
+#define INITIAL_CAPACITY 10
+
+struct Contact{
+    char first_name[50];
+    char last_name[50];
+    char phone[20];
+    char email[100];
+    char address[200];
+    char note[500];
+    time_t date_added;
+};
+
+struct ContactArray{
+    struct Contact *data;
+    int size;
+    int capacity;
+};
+
 #endif
